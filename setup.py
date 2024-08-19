7e02377ea93b491b7bca5ede176ff2adf4dcc471
Ben Parks <bnprks+git@gmail.com>
2024-08-19 13:23:18 -0700

    Add windows compile flag

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from glob import glob
 from setuptools import setup
 
-# Available at setup time due to pyproject.toml
-from pybind11.setup_helpers import Pybind11Extension, ParallelCompile
+import platform
 
+# Available at setup time due to pyproject.toml
+from pybind11.setup_helpers import Pybind11Extension
+
+if platform.system() == "Windows":
+    extra_compile_args = ["/std:c++17"]
+else:
+    extra_compile_args = ["-std=c++17"] 
 
 ext_modules = [
     Pybind11Extension(
         "example_package_1_bnprks.cpp",
         sorted(glob("src/*.cpp")),  # Sort source files for reproducibility
-        extra_compile_args = [
-            "-std=c++17"
-        ]
+        extra_compile_args = extra_compile_args
     ),
 ]
 
